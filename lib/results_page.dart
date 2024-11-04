d109648150f651a52829eef0961e1c09eac84374
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2024-11-04 20:28:34 +0200

    result page UI 1

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+import 'widgets.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -6,8 +8,52 @@ class ResultsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI Calculator'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
-      body: Text('Results Page'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            'Your Result',
+            style: TextStyle(fontSize: 50.0),
+          ),
+          const Expanded(
+            child: MyContainer(
+              childWidget: SizedBox.expand(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('OVERWEIGHT')
+                  ],
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      const InputPage(title: 'BMI Calculator'),
+                ),
+              );
+            },
+            child: Container(
+              color: Theme.of(context).colorScheme.tertiaryContainer,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80,
+              child: Center(
+                child: Text(
+                  "RE-CALCULATE",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
